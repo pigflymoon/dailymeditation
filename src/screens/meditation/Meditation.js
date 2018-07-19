d232ef3ee5904bca50bc01872f64ebe5497a2db7
pigflymoon <pplflymoon2015@gmail.com>
2018-07-19 12:24:10 +1200

    change icon to text for slide tab bar

--- src/screens/meditation/Meditation.js
+++ src/screens/meditation/Meditation.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet, ScrollView, StatusBar} from 'react-native';
-import baseStyle from '../../styles/base';
-import screenStyle from '../../styles/screen';
-
+import {View, Text, Image, StyleSheet, ScrollView, StatusBar,TouchableHighlight} from 'react-native';
 import ScrollableTabView, {ScrollableTabBar,} from 'react-native-scrollable-tab-view';
+
 import SlideTabBar from './SlideTabBar';
 import GridViewDemo from './GridViewDemo';
+
+import baseStyle from '../../styles/base';
+import screenStyle from '../../styles/screen';
 import sliderTabStyle from '../../styles/slideTab';
+
+
 
 export default class Meditation extends Component {
 
+    renderTab=(name, page, isTabActive, onPressHandler, onLayoutHandler)=> {
+        console.log('page',page)
+        return <TouchableHighlight
+            key={`${name}_${page}`}
+            onPress={() => onPressHandler(page)}
+            onLayout={onLayoutHandler}
+            style={{flex: 1, width: 100, }}
+            underlayColor="#aaaaaa"
+        >
 
+            <Text>{`${name}-test`}</Text>
+        </TouchableHighlight>;
+
+
+
+    }
     render() {
         return (
             <View style={[baseStyle.container,screenStyle.screenBgPurple]}>
@@ -18,25 +36,25 @@ export default class Meditation extends Component {
                     initialPage={0}
                     renderTabBar={() => <SlideTabBar />}
                 >
-                    <ScrollView tabLabel="ios-paper" style={sliderTabStyle.tabView}>
+                    <ScrollView tabLabel="All"  style={sliderTabStyle.tabView}>
                         <GridViewDemo/>
                     </ScrollView>
-                    <ScrollView tabLabel="ios-people" style={sliderTabStyle.tabView}>
+                    <ScrollView tabLabel="Meditation"  style={sliderTabStyle.tabView}>
                         <View style={sliderTabStyle.card}>
                             <Text>Friends</Text>
                         </View>
                     </ScrollView>
-                    <ScrollView tabLabel="ios-chatboxes" style={sliderTabStyle.tabView}>
+                    <ScrollView tabLabel="Anxiety" style={sliderTabStyle.tabView}>
                         <View style={sliderTabStyle.card}>
                             <Text>Messenger</Text>
                         </View>
                     </ScrollView>
-                    <ScrollView tabLabel="ios-notifications" style={sliderTabStyle.tabView}>
+                    <ScrollView tabLabel="Focus"  style={sliderTabStyle.tabView}>
                         <View style={sliderTabStyle.card}>
                             <Text>Notifications</Text>
                         </View>
                     </ScrollView>
-                    <ScrollView tabLabel="ios-list" style={sliderTabStyle.tabView}>
+                    <ScrollView tabLabel="Stresst"  style={sliderTabStyle.tabView}>
                         <View style={sliderTabStyle.card}>
                             <Text>Other nav</Text>
                         </View>
--- src/screens/meditation/SlideTabBar.js
+++ src/screens/meditation/SlideTabBar.js
@@ -5,7 +5,6 @@ import {
     TouchableOpacity,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-// import {Icon} from 'react-native-elements';
 import sliderTabStyle from '../../styles/slideTab';
 import colorStyle from '../../styles/colors';
 class SlideTabBar extends React.Component {
@@ -16,44 +15,14 @@ class SlideTabBar extends React.Component {
         this.icons = [];
     }
 
-    componentDidMount() {
-        // this._listener = this.props.scrollValue.addListener(this.setAnimationValue.bind(this));
-    }
-
-    setAnimationValue({value,}) {
-        this.icons.forEach((icon, i) => {
-            const progress = (value - i >= 0 && value - i <= 1) ? value - i : 1;
-            icon.setNativeProps({
-                style: {
-                    color: this.iconColor(progress),
-                },
-            });
-        });
-    }
-
-    //color between rgb(59,89,152) and rgb(204,204,204)
-    iconColor(progress) {
-        const red = 59 + (204 - 59) * progress;
-        const green = 89 + (204 - 89) * progress;
-        const blue = 152 + (204 - 152) * progress;
-        return `rgb(${red}, ${green}, ${blue})`;
-    }
-
     render() {
-
+        console.log('this.props',this.props)
         return <View style={[sliderTabStyle.tabs, this.props.style]}>
             {this.props.tabs.map((tab, i) => {
+
                 return <TouchableOpacity key={tab} onPress={() => this.props.goToPage(i)}
                                          style={[sliderTabStyle.tab]}>
-                    <Ionicons
-                        iconStyle={[sliderTabStyle.iconContainer]}
-                        name={this.props.activeTab === i?tab:`${tab}-outline`}
-                        size={30}
-                        color={this.props.activeTab === i?colorStyle.orange:'rgb(204,204,204)'}
-                        ref={(icon) => {
-                        this.icons[i] = icon;
-                    }}
-                    />
+                    <Text style={{color:this.props.activeTab === i?colorStyle.orange:'rgb(204,204,204)'}}> {tab}</Text>
                 </TouchableOpacity>;
             })}
         </View>;
