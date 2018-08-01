421ee6b09878aaa4f370f88053b27fe8c8b5863b
pigflymoon <pplflymoon2015@gmail.com>
2018-08-01 19:11:06 +1200

    update project name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import App from './App';
 import {name as appName} from './app.json';
 // import baseStyle from './src/styles/base';
 import colors from './src/styles/colors';
-import TrackPlayer from 'react-native-track-player';
+
 
 // import playHander from './src/utils/playerHandler';
 // import PlayerStore, { playbackStates } from './src/stores/Player';
@@ -61,30 +61,3 @@ class Root extends Component {
 
 AppRegistry.registerComponent(appName, () => Root);
 
-// TrackPlayer.registerEventHandler(async (data) => {
-//     console.log('##########data  type is ',data.type)
-//
-//     if (data.type === 'playback-track-changed') {
-//         if (data.nextTrack) {
-//             const track = await TrackPlayer.getTrack(data.nextTrack);
-//             TrackStore.title = track.title;
-//             TrackStore.artist = track.artist;
-//             TrackStore.artwork = track.artwork;
-//         }else{
-//             console.log('player reset!!')
-//             // TrackPlayer.reset();
-//         }
-//     } else if(data.type == 'remote-play') {
-//         TrackPlayer.play()
-//     } else if(data.type == 'remote-pause') {
-//         TrackPlayer.pause()
-//     } else if(data.type == 'remote-next') {
-//         TrackPlayer.skipToNext()
-//     } else if(data.type == 'remote-previous') {
-//         TrackPlayer.skipToPrevious()
-//     } else if (data.type === 'playback-state') {
-//         console.log('##########playbackState is ',data.state)
-//
-//         PlayerStore.playbackState = data.state;
-//     }
-// });
