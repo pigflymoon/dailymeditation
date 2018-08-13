611f23846ff581b279cb23c4cd2d2971f34fba51
pigflymoon <pplflymoon2015@gmail.com>
2018-08-13 15:47:43 +1200

    move  meditation type out of meditation category

--- src/utils/FetchAudiosByApi.js
+++ src/utils/FetchAudiosByApi.js
@@ -26,12 +26,15 @@ export function getAudiosByCategoryAndType(category = 'beginner', type = '') {
                     resolve(audiosArray);
                 });
             } else {//
-                db.ref().child(`${category}/${type}`).once("value", function (snapshot) {
+                db.ref().child(`${type}`).once("value", function (snapshot) {
                     var downloadAudios = snapshot.val(), audiosArray = [];
                     console.log('********88downloadAudios are**********: ', downloadAudios)
                     snapshot.forEach(function (childSnap) {
                         console.log('child snap is ', childSnap.val());
-                        audiosArray.push(childSnap.val())
+                        childSnap.forEach(function (snap) {
+                            console.log('snap is ', snap.val());
+                            audiosArray.push(snap.val())
+                        })
                     })
                     console.log('audiosArray is ', audiosArray)
                     resolve(audiosArray);
